6cf6f5d37191eba0bea8f975646da266de67d1fc
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-06-28 18:49:52 +0200

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -6,18 +6,48 @@ window.mouseClicked = mouseClicked;
 
 angleMode(DEGREES);
 
+let imgTestBild;
+let testSound;
+
+let testSoundIsActive = true;
+
 /*Seitenverhätnis 16:9 => Berechnung width=height * (16 / 9); height=height; 
 Vorteil: Sowohl im Normalmodus, als auch im Vollbildmodus einsehbar. 
 Im Normalmodus musste man auf die Tabs aufpassen..
 Sollte es nicht funktioneren, dann Format: 1200, 675 oder 1080. 608 (Seitenverhältnise =16:9)*/
 let newWidth = height * (16 / 9);
 
-let imgTestBild;
-let testSound;
+let averiaSansLibreBold;
+let averiaSansLibreBoldItalic;
+let averiaSansLibreItalic;
+let averiaSansLibreLight;
+let averiaSansLibreLightItalic;
+let averiaSansLibreRegular;
 
-let testSoundIsActive = true;
+function loadAveriaFonts() {
+  averiaSansLibreBold = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-Bold.ttf"
+  );
+  averiaSansLibreBoldItalic = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-BoldItalic.ttf"
+  );
+  averiaSansLibreItalic = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-Italic.ttf"
+  );
+  averiaSansLibreLight = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-Light.ttf"
+  );
+  averiaSansLibreLightItalic = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-LightItalic.ttf"
+  );
+  averiaSansLibreRegular = loadFont(
+    "./assets/fonts/averia/AveriaSansLibre-Regular.ttf"
+  );
+}
 
 function preload() {
+  loadAveriaFonts();
+
   imgTestBild = loadImage("./assets/img/testBild.png");
   testSound = loadSound("./Playground.mp3");
 }
@@ -34,9 +64,15 @@ function loadingScreen() {}
 // let button = new Button(300, 300, 300, 80);
 
 function starScreen() {
+  push();
   image(imgTestBild, 0, 0, newWidth, height);
   // button.display();
   // button.hitTestCustom();
+
+  textFont(averiaSansLibreRegular);
+  textSize(80);
+  text("Metanoia", 300, 300);
+  pop();
 }
 
 function draw() {
