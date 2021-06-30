cdbbf0832942494383a5d3dd291f54e10c369176
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-06-30 23:43:57 +0200

    Create sign.js

---
+++ classes/sign.js
@@ -0,0 +1,30 @@
+export default class Sign {
+  constructor(signX, signY, signWidth, signHeight, signText) {
+    this.signX = signX;
+    this.signY = signY;
+    this.signWidth = signWidth;
+    this.signHeight = signHeight;
+    this.signText = signText;
+  }
+
+  display(fontStyle) {
+    push();
+    translate(this.signX, this.signY);
+    fill(243, 233, 214);
+    noStroke();
+    rect(
+      -this.signWidth / 2,
+      -this.signHeight / 2,
+      this.signWidth,
+      this.signHeight
+    );
+
+    fill(0, 0, 0);
+    textAlign(CENTER, CENTER);
+    textFont(fontStyle);
+    textSize(15);
+    text(this.signText, 0, 0);
+
+    pop();
+  }
+}
