ddbca13d40664787fdb1f709d3713721e63e7157
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-07-10 00:21:26 +0200

    change name

--- classes/parameter.js
+++ classes/parameter.js
@@ -1,48 +1,47 @@
 export default class Parameter {
-    constructor(x, y, value, maximum){
-        this.x = x;
-        this.y = y;
-        this.value = value;
-        this.maximum = maximum;
-        this.width = 0;
+  constructor(x, y, value, maximum) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.maximum = maximum;
+    this.parameterWidth = 0;
+  }
+
+  small() {
+    if (this.parameterWidth < 40) {
+      return true;
+    }
+  }
+
+  display() {
+    //filling
+    if (this.small()) {
+      push();
+      noStroke();
+      fill(255);
+      ellipse(this.x + 20, this.y + 20, this.parameterWidth);
+      pop();
+    } else {
+      push();
+      noStroke();
+      fill(255);
+      rect(this.x, this.y, this.parameterWidth, 40, 30);
+      pop();
     }
 
-    small(){
-        if(this.width < 40){
-            return true;
-        }
-    }
+    //shell
+    push();
+    strokeWeight(3);
+    noFill();
+    rect(this.x, this.y, 350, 40, 30);
+    pop();
+  }
 
-    display (){
-        //filling
-        if(this.small()){
-            push();
-            noStroke();
-            fill(255);
-            ellipse(this.x + 20, this.y + 20, this.width);
-            pop();
-        } else {
-
-        push();
-        noStroke();
-        fill(255);
-        rect(this.x, this.y, this.width, 40, 30);
-        pop();
-        }
-        
-        //shell
-        push();
-        strokeWeight(3);
-        noFill();
-        rect(this.x, this.y, 350, 40, 30);
-        pop();
-    }
-
-    parameterAnimation(){
-        gsap.to(this, {
-            duration: 1.5,
-            ease: "linear",
-            width: (350 / this.maximum) * this.value,
-            });
-    }
+  parameterAnimation() {
+    gsap.to(this, {
+      duration: 1.5,
+      ease: "linear",
+      parameterWidth: (350 / this.maximum) * this.value,
+    });
+  }
 }
