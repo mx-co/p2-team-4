21b9db0d09aaa6e1ac2ddfd96c97e9408c88d75c
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-07-10 22:10:24 +0200

    Update guideScreen.js

--- guideScreen.js
+++ guideScreen.js
@@ -2,6 +2,7 @@ import {
   averiaSansLibreBold,
   averiaSansLibreRegular,
   greenCircle,
+  argumentVideo,
   guideScreenVideo,
 } from "./preload.js";
 import Button from "./classes/button.js";
@@ -9,9 +10,9 @@ import Button from "./classes/button.js";
 let startGuideButton = new Button(
   900,
   675 / 2,
-  400,
-  400,
-  "hallo",
+  300,
+  300,
+  "Start",
   true,
   0,
   0,
@@ -19,16 +20,33 @@ let startGuideButton = new Button(
 );
 
 export function guideScreen() {
-  // background(100, 100, 200);
-  image(guideScreenVideo, 0, 0, 1200, 675);
+  push();
 
-  startGuideButton.display(averiaSansLibreBold, greenCircle, 0.6);
+  image(guideScreenVideo, 0, 0, 1200, 675);
+  // background(200, 200, 100);
+
+  startGuideButton.display(averiaSansLibreBold, greenCircle, 0.4);
+
+  // testLeading();
+
+  textFont(averiaSansLibreRegular);
+  textSize(20);
+  text(
+    "Willkommen im verwunschenen Tanoiawald, \nein Ort der vollkommenen Urteilslosigkeit. \nHier lebt eine besondere Spezies, die Tüdelüs genannt wird. \nSie sind kleine romantische Fabelwesen. Du begibst dich nun auf eine Reise, \nin der du dich in das Leben eines Tüdeldüs hineinversetzt, das gerade \neine Trennung hinter sich hat. Genau wie du durchlebt es \nnun eine schwierige und anstrengende Zeit, vollgepackt \nmit lauter verschiedener Emotionen. In dieser \nSimulation geht es um deine auf diesen Gefühlen \nbasierenden Entscheidungen also entscheide \nzunächst aus dem Bauch heraus.",
+    200,
+    150
+  );
+
+  pop();
 }
 
 export function mouseClickedForGuideScreen() {
+  // zum testen
   guideScreenVideo.loop();
 
   if (startGuideButton.hitTestCircle()) {
+    argumentVideo.loop();
+
     return true;
   }
 }
