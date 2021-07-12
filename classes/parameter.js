3e165de0703e88d38a284107f47a1ea5d1d9cdfe
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-07-12 14:18:57 +0200

    add bright dark modus

--- classes/parameter.js
+++ classes/parameter.js
@@ -14,34 +14,46 @@ export default class Parameter {
     }
   }
 
-  display(font) {
+  display(font, colorModus) {
     push();
     //filling
-    if (this.small()) {
-      push();
-      noStroke();
-      fill(255, 255, 22, 100);
-      ellipse(this.x + 20, this.y + 20, this.parameterWidth);
-      pop();
-    } else {
-      push();
-      noStroke();
-      fill(255);
-      rect(this.x, this.y, this.parameterWidth, 40, 30);
-      pop();
-    }
 
-    //shell
+    if (colorModus === "bright") {
+      stroke(141, 180, 127);
+      fill(141, 180, 127);
+    } else if (colorModus === "dark") {
+      stroke(100, 135, 160);
+      fill(100, 135, 160);
+    }
     push();
-    strokeWeight(3);
-    noFill();
-    rect(this.x, this.y, 350, 40, 30);
+    noStroke();
+    if (this.small()) {
+      ellipse(this.x + 20, this.y + 20, this.parameterWidth);
+    } else {
+      noStroke();
+      rect(this.x, this.y, this.parameterWidth, 40, 30);
+    }
     pop();
 
+    //shell
+    strokeWeight(3);
+    // stroke(100, 135, 160);
     // text
+    push();
+    noStroke();
+    if (colorModus === "bright") {
+      fill(62, 58, 53);
+    } else if (colorModus === "dark") {
+      fill(243, 233, 214);
+    }
     textFont(font);
+    textSize(20);
     textAlign(LEFT, CENTER);
-    text(this.text, this.x + 20, this.y + 20);
+    text(this.text, this.x + 20, this.y + 18);
+    pop();
+
+    noFill();
+    rect(this.x, this.y, 350, 40, 30);
 
     pop();
   }
