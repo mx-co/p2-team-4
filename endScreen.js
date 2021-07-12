44ab071902b315c00a200238ed8dfbef22937fba
Trimmm <max.m.mittelstaedt@stud.h-da.de>
2021-07-12 14:20:49 +0200

    update import

--- endScreen.js
+++ endScreen.js
@@ -1,59 +1,79 @@
 import {
   averiaSansLibreBold,
   averiaSansLibreRegular,
+  blueCircle,
   buttonClickSound,
   greenCircle,
   negativeEndingVideo,
   positiveEndingVideo,
 } from "./preload.js";
 import Parameter from "./classes/parameter.js";
-import {
-  acceptanceCounter,
-  angerCounter,
-  bargainingCounter,
-  denialCounter,
-  depressionCounter,
-} from "./gameScreen.js";
+import { getCounters } from "./gameScreen.js";
 import Button from "./classes/button.js";
 
-let denial = new Parameter(30, 15, denialCounter, 12, "Verweigerung");
-let anger = new Parameter(30, 60, angerCounter, 12, "Zorn");
-let bargaining = new Parameter(30, 105, bargainingCounter, 12, "Verhandeln");
-let depression = new Parameter(30, 150, depressionCounter, 12, "Depression");
-let acceptance = new Parameter(30, 195, acceptanceCounter, 12, "Akzeptanz");
+let denial;
+let anger;
+let bargaining;
+let depression;
+let acceptance;
+
+let colorModus;
+let setColorCircle;
+let init = true;
+
+function setParameters() {
+  denial = new Parameter(30, 15, getCounters()[0], 5, "Verweigerung");
+  anger = new Parameter(30, 60, getCounters()[1], 3, "Zorn");
+  bargaining = new Parameter(30, 105, getCounters()[2], 5, "Verhandeln");
+  depression = new Parameter(30, 150, getCounters()[3], 4, "Depression");
+  acceptance = new Parameter(30, 195, getCounters()[4], 11, "Annahme");
+}
 
 let againButton = new Button(
-  1050,
-  675 / 2,
-  200,
-  200,
+  1120,
+  570,
+  300,
+  300,
   "Erneut spielen",
   true,
-  0,
-  0,
+  -20,
+  -10,
   20
 );
 
 function fiveParameter() {
-  denial.display(averiaSansLibreRegular);
-  anger.display(averiaSansLibreRegular);
-  bargaining.display(averiaSansLibreRegular);
-  depression.display(averiaSansLibreRegular);
-  acceptance.display(averiaSansLibreRegular);
+  denial.display(averiaSansLibreRegular, colorModus);
+  anger.display(averiaSansLibreRegular, colorModus);
+  bargaining.display(averiaSansLibreRegular, colorModus);
+  depression.display(averiaSansLibreRegular, colorModus);
+  acceptance.display(averiaSansLibreRegular, colorModus);
 }
 
 export function endScreen() {
   push();
 
-  // bedingung nicht richtig
-  if (denialCounter < acceptanceCounter) {
+  if (init) {
+    setParameters();
+    init = false;
+  }
+
+  textFont(averiaSansLibreRegular);
+  textSize(20);
+  // text gfehlt
+  if (getCounters()[4] >= getCounters()[5] / 2) {
     image(positiveEndingVideo, 0, 0, 1200, 675);
+    colorModus = "bright";
+    setColorCircle = greenCircle;
+    text("hurra", 300, 300);
   } else {
     image(negativeEndingVideo, 0, 0, 1200, 675);
+    colorModus = "dark";
+    setColorCircle = blueCircle;
+    text("on NO", 300, 300);
   }
-  background(100, 230, 100);
+
   fiveParameter();
-  againButton.display(averiaSansLibreBold, greenCircle, 0.3);
+  againButton.display(averiaSansLibreBold, setColorCircle, 0.45);
   pop();
   filling();
 }
@@ -71,8 +91,9 @@ function filling() {
 }
 
 export function mouseClickedForEndScreen() {
-  positiveEndingVideo.loop();
-  negativeEndingVideo.loop();
+  // fürs Testen
+  // positiveEndingVideo.loop();
+  // negativeEndingVideo.loop();
 
   if (againButton.hitTestCircle()) {
     denial.parameterAnimationReset();
@@ -81,6 +102,7 @@ export function mouseClickedForEndScreen() {
     depression.parameterAnimationReset();
     acceptance.parameterAnimationReset();
     fillingIsActive = true;
+    init = true;
     buttonClickSound.play();
     return true;
   }
